cc5a34da1f936d7cd691544453dbbe097da6750f
edent11 <eden.tamar2@gmail.com>
2023-12-06 19:44:32 +0200

    update passed alarms

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { React } from 'react';
-import { StatusBar, SafeAreaView, ScrollView, View } from 'react-native';
+import { React, useRef, useState, useEffect } from 'react';
+import { StatusBar, SafeAreaView, ScrollView, View, AppState } from 'react-native';
 import Tabs from './components/Tabs';
 import { Audio } from 'expo-av';
 import { sound } from './assets/globals'
 
 export default function App() {
 
+  // const appState = useRef(AppState.currentState);
+  // const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  // useEffect(() => {
+  //   const subscription = AppState.addEventListener('change', nextAppState => {
+  //     if (
+  //       appState.current.match(/inactive|background/) &&
+  //       nextAppState === 'active'
+  //     )
+  //       console.log('App has come to the foreground!');
+
+
+  //     appState.current = nextAppState;
+  //     setAppStateVisible(appState.current);
+  //     console.log('AppState', appState.current);
+  //   });
+
+  //   return () => {
+  //     subscription.remove();
+  //   };
+  // }, []);
 
 
   return (
@@ -28,7 +49,9 @@ export default function App() {
           // }  */}
 
 
-        <Tabs />
+        <Tabs
+        // appIsForeground={appState.current}
+        />
 
       </NavigationContainer >
 
